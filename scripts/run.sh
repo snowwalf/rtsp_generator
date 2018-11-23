8c68559edd651c9ca2e08b2ff6227a58cc6ef925
zhangji <zhangji@qiniu.com>
2018-11-23 16:44:21 +0800

    add use ts format
    Signed-off-by: zhangji <zhangji@qiniu.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,4 @@ ffserver &
 
 sleep 3
 
-ffmpeg -re -fflags +genpts -stream_loop -1 -i /data/video.mp4 -qmax 50 -qcomp 0 http://localhost:18090/feed.ffm
+ffmpeg -re -fflags +genpts -stream_loop -1 -i /data/video.ts -qmax 50 -qcomp 0 http://localhost:18090/feed.ffm
